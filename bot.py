944f539e1abd3c7cf5418e86dac6669cfa96ee5c
Denis <18denis99@rambler.ru>
2018-11-11 16:12:27 +0300

    Add commit message

--- bot.py
+++ bot.py
@@ -39,6 +39,61 @@ class VKBot:
         self.upload = VkUpload(self.vk_session)
         self.long_poll = VkLongPoll(self.vk_session)
 
+    def damerau_levenshtein_distance(self, s1, s2):
+       d = {}
+       lenstr1 = len(s1)
+       lenstr2 = len(s2)
+       for i in range(-1, lenstr1 + 1):
+           d[(i, -1)] = i + 1
+       for j in range(-1, lenstr2 + 1):
+           d[(-1, j)] = j + 1
+       for i in range(lenstr1):
+           for j in range(lenstr2):
+               if s1[i] == s2[j]:
+                   cost = 0
+               else:
+                   cost = 1
+               d[(i, j)] = min(
+                   d[(i - 1, j)] + 1,  # deletion
+                   d[(i, j - 1)] + 1,  # insertion
+                   d[(i - 1, j - 1)] + cost,  # substitution
+               )
+               if i and j and s1[i] == s2[j - 1] and s1[i - 1] == s2[j]:
+                   d[(i, j)] = min(d[(i, j)], d[i - 2, j - 2] + cost)  # transposition
+       return d[lenstr1 - 1, lenstr2 - 1]
+
+    def unknow_message(self, message, vk):
+        vk.messages.send(user_id=message.user_id, message=u"""
+                Кажется, сообщение с ошибками или не относится к расписанию.
+                """)
+
+    def get_data_from_message(self, messages_with_date):
+        first_months = {'ФЕВРАЛЯ':'2', 'МАРТА':'3','АПРЕЛЯ':'4','МАЯ':'5'}
+        second_months = {'СЕНТРЯБРЯ':'9', 'ОКТЯБРЯ':'10','НОЯБРЯ':'11','ДЕКАБРЯ':'12'}
+        for message in messages_with_date:
+            match = re.search(r'\d+', message)
+            match2 = re.search(r'\d+.\d+', message)
+            if match2!=None:
+                try:
+                    date = datetime.datetime.strptime(match2.group()+'.2018', '%d.%m.%Y').date()
+                    if date.month > 8 and date.month <= 12: 
+                        return date
+                except:
+                    print('ОШИБКА1')
+            elif match!=None:
+                break
+
+        for message in messages_with_date:   
+            if message.upper() in second_months:
+                month = second_months[message.upper()]
+                try:
+                    date = datetime.datetime.strptime(match.group()+'.'+month+'.'+'2018', '%d.%m.%Y').date()
+                    return date
+                except:
+                    print('ОШИБКА2')
+                break
+        return False
+
     def __command_handler__(self, commands, handler):
         """
         Run user function if message contain a commands
@@ -46,11 +101,26 @@ class VKBot:
         :param handler: function, that should run if message contain a command
         """
         message_set = self.event.text.split(u' ')
-        for command in commands:
-            # print(re.search(r'(\d+/\d+/\d+)', command))
-            if command in message_set:
+        for command in commands:           
+            for message in message_set:
+                if handler.__name__=='on_date' and self.get_data_from_message(message_set)!=False:
+                    handler(self.event, self.vk, self.get_data_from_message(message_set))
+                    return 1      
+                distance = len(message)
+                d = self.damerau_levenshtein_distance(message, command)
+                if d < distance:
+                    distance = d
+                    key = command
+                    if distance == 0:
+                        handler(self.event, self.vk)
+                        return 1
+            if distance < len(message)*0.4 and message not in ["Неделя", "неделя", "нед"]:
                 handler(self.event, self.vk)
-                break
+                return 1
+                
+            # if command in message_set:
+            #     handler(self.event, self.vk)
+            #     return 1
 
     def __query_manager__(self, queryset):
         """
@@ -58,7 +128,10 @@ class VKBot:
         :param queryset: list of commands and hanlers. For example [["command", handler], ...]
         """
         for item in queryset:
-            self.__command_handler__(item[0], item[1])
+            print(item[1].__name__ )
+            if (self.__command_handler__(item[0], item[1])) == 1:
+                return 1
+
 
     def run(self, query):
         """
@@ -68,8 +141,10 @@ class VKBot:
         for event in self.long_poll.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
                 self.event = event
-                self.__query_manager__(query)
-                break
+                if (self.__query_manager__(query))==1:
+                    break
+                else:
+                    self.unknow_message(self.event, self.vk)
 
     def get_user_id(self):
         for event in self.long_poll.listen():
--- example.py
+++ example.py
@@ -8,18 +8,14 @@ import re
 conn = pymysql.connect(host='127.0.0.1', port=3307, user='root', passwd='denis', db='timetable')
 cur = conn.cursor()
 
-data_now = datetime.datetime.utcnow()
-
 lecture_phisic = [1, 5, 9, 13]
 laba_phisic = [2, 6, 10, 14]
 
 
 def get_group(vk_id):
 	req = cur.execute("""SELECT * FROM users WHERE vk_id = (%s)""", str(vk_id))	
-	print(req)
 	for el in cur:
 		group = el[2]
-	print(group)
 	return group
 
 
@@ -41,49 +37,49 @@ def create_message(request):
 	return mes
 
 
-def monday(data, vk_id):
+def monday(data, vk_id, mes_date=''):
 	if get_week(data)%2!=0:
 		req = cur.execute("""SELECT Number_lesson, Class, Start_lesson, End_lesson, Subject, Type_lesson  FROM time_table WHERE Day_of_week='ПОНЕДЕЛЬНИК' AND Subject IS NOT NULL AND WEEK = 'I' AND Group_ID = %s""", get_group(vk_id))
 	else:
 		req = cur.execute("""SELECT Number_lesson, Class, Start_lesson, End_lesson, Subject, Type_lesson  FROM time_table WHERE Day_of_week='ПОНЕДЕЛЬНИК' AND Subject IS NOT NULL AND WEEK = 'II' AND Group_ID = %s""", get_group(vk_id))
-	message = 'Пары на понедельник (' + str(data.day) + '.' + str(data.month) + ')' + ':\n\n' + create_message(req)	
+	message = 'Пары на понедельник ' + mes_date + ':\n\n' + create_message(req)	
 	return message
 
 
 
-def tuesday(data, vk_id):
+def tuesday(data, vk_id, mes_date=''):
 	if get_week(data)%2!=0:
 		req = cur.execute("SELECT Number_lesson, Class, Start_lesson, End_lesson, Subject, Type_lesson  FROM time_table WHERE Day_of_week='ВТОРНИК' AND Subject IS NOT NULL AND WEEK = 'I' AND Group_ID = %s", get_group(vk_id))
 	else:
 		req = cur.execute("SELECT Number_lesson, Class, Start_lesson, End_lesson, Subject, Type_lesson  FROM time_table WHERE Day_of_week='ВТОРНИК' AND Subject IS NOT NULL AND WEEK = 'II' AND Group_ID = %s", get_group(vk_id))
-	message = 'Пары на вторник (' + str(data.day) + '.' + str(data.month) + ')' + ':\n\n' + create_message(req)	
+	message = 'Пары на вторник ' + mes_date + ':\n\n' + create_message(req)	
 	return message
 
-def wednesday(data, vk_id):
+def wednesday(data, vk_id, mes_date=''):
 	if get_week(data)%2!=0:
 		req = cur.execute("SELECT Number_lesson, Class, Start_lesson, End_lesson, Subject, Type_lesson  FROM time_table WHERE Day_of_week='СРЕДА' AND Subject IS NOT NULL AND WEEK = 'I' AND Group_ID = %s", get_group(vk_id))
 	else:
 		req = cur.execute("SELECT Number_lesson, Class, Start_lesson, End_lesson, Subject, Type_lesson  FROM time_table WHERE Day_of_week='СРЕДА' AND Subject IS NOT NULL AND WEEK = 'II' AND Group_ID = %s", get_group(vk_id))
-	message = 'Пары на среду (' + str(data.day) + '.' + str(data.month) + ')' + ':\n\n' + create_message(req)	
+	message = 'Пары на среду ' + mes_date + ':\n\n' + create_message(req)	
 	return message	
 
-def thursday(data, vk_id):
+def thursday(data, vk_id, mes_date=''):
 	if get_week(data)%2!=0:
 		req = cur.execute("SELECT Number_lesson, Class, Start_lesson, End_lesson, Subject, Type_lesson  FROM time_table WHERE Day_of_week='ЧЕТВЕРГ' AND Subject IS NOT NULL AND WEEK = 'I' AND Group_ID = %s", get_group(vk_id))
 	else:
 		req = cur.execute("SELECT Number_lesson, Class, Start_lesson, End_lesson, Subject, Type_lesson  FROM time_table WHERE Day_of_week='ЧЕТВЕРГ' AND Subject IS NOT NULL AND WEEK = 'II' AND Group_ID = %s", get_group(vk_id))
-	message = 'Пары на четверг (' + str(data.day) + '.' + str(data.month) + ')' + ':\n\n' + create_message(req)	
+	message = 'Пары на четверг ' + mes_date + ':\n\n' + create_message(req)	
 	return message
 
-def friday(data, vk_id):
+def friday(data, vk_id, mes_date=''):
 	if get_week(data)%2!=0:
 		req = cur.execute("SELECT Number_lesson, Class, Start_lesson, End_lesson, Subject, Type_lesson  FROM time_table WHERE Day_of_week='ПЯТНИЦА' AND Subject IS NOT NULL AND WEEK = 'I' AND Group_ID = %s", get_group(vk_id))
 	else:
 		req = cur.execute("SELECT Number_lesson, Class, Start_lesson, End_lesson, Subject, Type_lesson  FROM time_table WHERE Day_of_week='ПЯТНИЦА' AND Subject IS NOT NULL AND WEEK = 'II' AND Group_ID = %s", get_group(vk_id))
-	message = 'Пары на пятницу (' + str(data.day) + '.' + str(data.month) + ')' + ':\n\n' + create_message(req)	
+	message = 'Пары на пятницу ' + mes_date + ':\n\n' + create_message(req)	
 	return message
 
-def saturday(data, vk_id):
+def saturday(data, vk_id, mes_date=''):
 	if get_week(data)%2!=0:
 		if get_week(data) in lecture_phisic:
 			req = cur.execute("SELECT Number_lesson, Class, Start_lesson, End_lesson, Subject, Type_lesson  FROM time_table WHERE Day_of_week='СУББОТА' AND Subject IS NOT NULL AND WEEK = 'I' AND Group_ID = %s", get_group(vk_id))
@@ -94,111 +90,125 @@ def saturday(data, vk_id):
 			req = cur.execute("SELECT Number_lesson, Class, Start_lesson, End_lesson, Subject, Type_lesson  FROM time_table WHERE Day_of_week='СУББОТА' AND Subject IS NOT NULL AND WEEK = 'II' AND Type_lesson = 'пр' AND Group_ID = %s", get_group(vk_id))
 		else:
 			req = cur.execute("SELECT Number_lesson, Class, Start_lesson, End_lesson, Subject, Type_lesson  FROM time_table WHERE Day_of_week='СУББОТА' AND Subject IS NOT NULL AND WEEK = 'II' AND Group_ID = %s", get_group(vk_id))
-	message = 'Пары на субботу (' + str(data.day) + '.' + str(data.month) + ')' + ':\n\n' + create_message(req)	
+	message = 'Пары на субботу ' + mes_date + ':\n\n' + create_message(req)	
 	return message
 
-def send_monday(message, vk):
-	vk.messages.send(user_id=message.user_id, message=monday(data_now, str(message.user_id)))
+def send_monday(message, vk, correct_mes=''):
+	vk.messages.send(user_id=message.user_id, message=correct_mes + monday(datetime.datetime.utcnow(), message.user_id))
 
-def send_tuesday(message, vk):
-	vk.messages.send(user_id=message.user_id, message=tuesday(data_now, str(message.user_id)))
+def send_tuesday(message, vk, correct_mes=''):
+	vk.messages.send(user_id=message.user_id, message=correct_mes + tuesday(datetime.datetime.utcnow(), message.user_id))
 
-def send_wednesday(message, vk):
-	vk.messages.send(user_id=message.user_id, message=wednesday(data_now, str(message.user_id)))
+def send_wednesday(message, vk, correct_mes=''):
+	vk.messages.send(user_id=message.user_id, message=correct_mes + wednesday(datetime.datetime.utcnow(), message.user_id))
 
-def send_thursday(message, vk):
-	vk.messages.send(user_id=message.user_id, message=thursday(data_now, str(message.user_id)))
+def send_thursday(message, vk, correct_mes=''):
+	vk.messages.send(user_id=message.user_id, message=correct_mes + thursday(datetime.datetime.utcnow(), message.user_id))
 
-def send_friday(message, vk):
-	vk.messages.send(user_id=message.user_id, message=friday(data_now, str(message.user_id)))
+def send_friday(message, vk, correct_mes=''):
+	vk.messages.send(user_id=message.user_id, message=correct_mes + friday(datetime.datetime.utcnow(), message.user_id))
 
-def send_saturday(message, vk):
-	vk.messages.send(user_id=message.user_id, message=saturday(data_now, str(message.user_id)))
+def send_saturday(message, vk, correct_mes=''):
+	vk.messages.send(user_id=message.user_id, message=correct_mes + saturday(datetime.datetime.utcnow(), message.user_id))
 
 
-def get_weekday(day, vk_id):
+def get_weekday(day, mes_date, vk_id):
 	if day.weekday()==0:
-		return monday(day, vk_id)
+		return monday(day, vk_id, mes_date)
 	elif day.weekday()==1:
-		return tuesday(day, vk_id)
+		return tuesday(day, vk_id, mes_date)
 	elif day.weekday()==2:
-		return wednesday(day, vk_id)
+		return wednesday(day, vk_id, mes_date)
 	elif day.weekday()==3:
-		return thursday(day, vk_id)
+		return thursday(day, vk_id, mes_date)
 	elif day.weekday()==4:
-		return friday(day, vk_id)
+		return friday(day, vk_id, mes_date)
 	elif day.weekday()==5:
-		return saturday(day, vk_id)
+		return saturday(day, vk_id, mes_date)
 	else:
 		mes_sun = 'В воскресенье выходной день!'
 		return mes_sun
 
-def today(message, vk):
-	# get_weekday(datetime.datetime.today())
-	vk.messages.send(user_id=message.user_id, message=get_weekday(datetime.datetime.today(), str(message.user_id)))
+def on_date(message, vk, date):
+	mes_date = '(' + str(date.day) + '.' + str(date.month) + ')'
+	vk.messages.send(user_id=message.user_id, message=get_weekday(date, mes_date,message.user_id))
 
-def tomorow(message, vk):
-	# data = datetime.datetime.today()+datetime.timedelta(days=1)
-	vk.messages.send(user_id=message.user_id, message=get_weekday(datetime.datetime.today()+datetime.timedelta(days=1), str(message.user_id)))
+def today(message, vk, correct_mes=''):
+	mes_date = '(' + str(datetime.datetime.today().day) + '.' + str(datetime.datetime.today().month) + ')'
+	vk.messages.send(user_id=message.user_id, message=correct_mes + get_weekday(datetime.datetime.today(), mes_date, message.user_id))
 
-def for_week(message, vk):
+def tomorow(message, vk, correct_mes=''):
+	date = datetime.datetime.today()+datetime.timedelta(days=1)
+	mes_date = '(' + str(date.day) + '.' + str(date.month) + ')'
+	vk.messages.send(user_id=message.user_id, message=correct_mes + get_weekday(date, mes_date, message.user_id))
+
+def for_week(message, vk, correct_mes=''):
 	data = datetime.datetime.today()
 	mes_week = '_________________________\n\n'
 	for i in range(7):
-		if get_weekday(data, str(message.user_id))!='В воскресенье выходной день!':
-			mes_week += get_weekday(data, str(message.user_id))  + '_________________________\n\n'
+		mes_date = '(' + str(data.day) + '.' + str(data.month) + ')'
+		if get_weekday(data, mes_date, message.user_id)!='В воскресенье выходной день!':
+			mes_week += get_weekday(data, mes_date, message.user_id)  + '_________________________\n\n'
 		data = data + datetime.timedelta(days=1) 
-	vk.messages.send(user_id=message.user_id, message=mes_week)
+	vk.messages.send(user_id=message.user_id, message=correct_mes + mes_week)
 
 
-def week(message, vk):
-	vk.messages.send(user_id=message.user_id, message='Сейчас ' + str(get_week(data_now)) + ' неделя.')
+def week(message, vk, correct_mes=''):
+	vk.messages.send(user_id=message.user_id, message=correct_mes + 'Сейчас ' + str(get_week(datetime.datetime.utcnow())) + ' неделя.')
 
+def teachers(message, vk, correct_mes=''):
+	req = cur.execute("SELECT DISTINCT Teachers FROM time_table WHERE Group_ID = %s AND Teachers IS NOT NULL", get_group(message.user_id))
+	teacher_list = ''
+	for row in cur:
+		teacher_list += row[0] + '\n'
+	vk.messages.send(user_id=message.user_id, message=correct_mes + teacher_list)
 
-def start(message, vk):
-    vk.messages.send(user_id=message.user_id, message=u"Начнем, пожалуй")
+def test(message, vk, correct_mes=''):
+	vk.messages.send(user_id=message.user_id, message=correct_mes + u"Работает")
 
-def random_habrahabr(message, vk):
-    vk.messages.send(user_id=message.user_id, message=u'https://habrahabr.ru/post/' + str(randint(100, 200000)) + u'/')
+def hello(message, vk, correct_mes=''):
+	vk.messages.send(user_id=message.user_id, message=correct_mes + u'Тебя нет в базе, введи свою группу!\n Группы, которые поддреживает бот:\n БББО-01-17\nБББО-02-17 ')
 
-def test(message, vk):
-	vk.messages.send(user_id=message.user_id, message=u"Работает")
+def list_comand(message, vk, correct_mes=''):
+	list_message = '''Вот что я умею:
+		Выводить расписание на определенный день недели
+		Выводить расписание на сегодня и завтра
+		Выводить расписание на всю неделю
+		Выводить номер недели
+		Выводить список преподавателей
+		__________________________________
 
-def hello(message, vk):
-	vk.messages.send(user_id=message.user_id, message=u'Тебя нет в базе, введи свою группу')
+		Для того чтобы посмотореть расписание другой группы напиши мне "Сменить" и затем введи нужную тебе группу!
 
+	'''
+	vk.messages.send(user_id=message.user_id, message=correct_mes + list_message)
 
-def start(message, vk):
+def start(message, vk, correct_mes=''):
 	vk_id = message.user_id
 	group = message.text
 	try:
 		cur.execute("""INSERT INTO users VALUES (NULL, %s, %s)""", ( vk_id, group))
 		conn.commit()
-		vk.messages.send(user_id=message.user_id, message=u'Я добавил тебя в базу, можешь приступать к работе!')
+		vk.messages.send(user_id=message.user_id, message=correct_mes + u'Я добавил тебя в базу, можешь приступать к работе!')
+		list_comand(message, vk)
 	except:	
 		conn.rollback()
-		vk.messages.send(user_id=message.user_id, message=u'Упс, что то пошло не так!')
+		vk.messages.send(user_id=message.user_id, message=correct_mes + u'Упс, что то пошло не так!')
 
-		# vk.messages.send(user_id=message.user_id, message=u'Я добавил тебя в базу, можешь приступать к работе!')
 
-def delete_user(message, vk):
+def delete_user(message, vk, correct_mes=''):
 	vk_id = message.user_id
 	try:
 		cur.execute("""DELETE FROM users WHERE vk_id = (%s)""", str(message.user_id))
 		conn.commit()
-		vk.messages.send(user_id=message.user_id, message=u'Смена группы произошла успешно!')
+		vk.messages.send(user_id=message.user_id, message=correct_mes + u'Смена группы произошла успешно!')
+		hello(message, vk)
 	except:	
 		conn.rollback()
-		vk.messages.send(user_id=message.user_id, message=u'Упс, что то пошло не так!')
-
-
-
-
-
-
+		vk.messages.send(user_id=message.user_id, message=correct_mes + u'Упс, что то пошло не так!')
 
 if __name__ == '__main__':
-	bot = VKBot(token='')
+	bot = VKBot(token='ad2782d4222562577747d80a4e616f6e8f9d566dfe73ca2e67656b3e2537e57c770fbce7bcc61073d86b5')
 	user_id = bot.get_user_id()
 	while True:
 		req = cur.execute("""SELECT vk_id FROM users WHERE vk_id = (%s)""", str(user_id))
@@ -209,21 +219,21 @@ if __name__ == '__main__':
 			user_id_db = 0		
 		if int(user_id_db) == user_id:
 			queryset = [
-			[[u"Погнали", u"погнали", u"лол", u"Лол"], start], 
-			[[u"Хабрахабр", ], random_habrahabr],
 			[[u"Понедельник", "пн","понедельник",], send_monday],
 			[[u"Вторник", "вт", "вторник",], send_tuesday],
 			[[u"Среда", "ср", "среда", "среду", "Среду",], send_wednesday],
 			[[u"Четверг", "чт", "четверг",], send_thursday],
 			[[u"Пятница", "пт", "пятницу", "пятница", "Пятинцу",], send_friday],
-			[[u"Суббота", "суб", "суббота", "Субботу", "субботу",], send_saturday],
+			[[u"Суббота", "суб", "суббота", "Субботу", "субботу", "сб",], send_saturday],
 			[[u"Сегодня", "сегодня",], today],
 			[[u"Завтра", "завтра"], tomorow],
 			[[u"Неделю", "неделю"], for_week],
 			[[u"Сменить",], delete_user],
 			[[r"[A-Z]{1}[a-z]{1}", r'\d+/\d+/\d+', r"1\s"], test],
 			[[u"Неделя", "неделя", "нед"], week],
-			[[u""], week]
+			[[u"преподы",], teachers],
+			[[u"info","Инфо","инфо","команды", "Комнадны"], list_comand],
+			[['1'], on_date]
 			]
 		else:
 			queryset = [
