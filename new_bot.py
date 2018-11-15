f67f2205fc9691d9ec033f31b4005d77e78cb91e
Denis <18denis99@rambler.ru>
2018-11-15 17:45:33 +0300

    Fix bag with notifications when add users

--- new_bot.py
+++ new_bot.py
@@ -276,7 +276,7 @@ def hello(message, vk):
 if __name__ == '__main__':
 	bot = VKBot(token='ad2782d4222562577747d80a4e616f6e8f9d566dfe73ca2e67656b3e2537e57c770fbce7bcc61073d86b5')	
 	while True:
-		if datetime.datetime.now().strftime('%H:%M:%S')=='14:45:00' and datetime.datetime.now().weekday()!=6:
+		if datetime.datetime.now().strftime('%H:%M:%S')=='14:48:00' and datetime.datetime.now().weekday()!=6:
 			print('WORK!')
 			cur.execute(u"""SELECT vk_id, group_id FROM users WHERE notifications='yes'""")
 			for el in cur:
